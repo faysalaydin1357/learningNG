021c32bc02ee43ee436b1f992f44ce7f2b14231a
Faysal Aydin <faysal.aydin@rabobank.com>
2020-01-05 11:48:38 +0100

    image showing filter showing

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -8,6 +8,11 @@ import {Component} from "@angular/core";
 })
 
 export class ProductListComponent{
+  pageTitle: string="Product List"
+  imageWidth: number = 50;
+  imageMargin: number =2;
+  showImage: boolean =false;
+  listFilter: string ='cart';
   products: any[] =[
     {
       "productId": 1,
@@ -60,5 +65,8 @@ export class ProductListComponent{
       "imageUrl": "assets/images/xbox-controller.png"
     }
   ];
-  pageTitle: string="Product List"
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
 }
